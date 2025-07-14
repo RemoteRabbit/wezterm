85b48b2d4838101d0ddf5d8c55523c4b34346d57
Tristan Jahnke <trjahnke@protonmail.com>
2025-07-14 15:49:47 -0500

    fix(background): Fix layering of background image

--- wezterm.lua
+++ wezterm.lua
@@ -1,35 +1,33 @@
 local wezterm = require("wezterm")
 
--- This table will hold the configuration.
 local config = {}
 
 if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
-config.default_prog = {'/bin/zsh', '-l'}
+config.default_prog = { "/bin/zsh", "-l" }
 
 config.background = {
 	{
 		-- source = { File = wezterm.config_dir .. "/backdrops/saturn-devouring-sons.jpeg" },
 		source = { File = wezterm.config_dir .. "/backdrops/planets.jpg" },
-    opacity = 0.9,
+		opacity = 0.9,
+		horizontal_align = "Center",
+		vertical_align = "Middle",
+		hsb = {
+			hue = 1.0,
+			saturation = 1.5,
+			brightness = 0.1,
+		},
 	},
-	-- {
-	-- 	-- source = { Color = colors.background },
-	-- 	height = "100%",
-	-- 	width = "100%",
-	-- 	opacity = 0.90,
-	-- },
 }
 
--- config.font = wezterm.font("JetBrains Mono")
--- config.font = wezterm.font("Fira Code")
-config.font = wezterm.font_with_fallback {
-  "FiraCode Nerd Font",
-  "JetBrainsMono Nerd Font",
-  "JetBrains Mono",
-}
+config.font = wezterm.font_with_fallback({
+	"FiraCode Nerd Font",
+	"JetBrainsMono Nerd Font",
+	"JetBrains Mono",
+})
 config.color_scheme = "Catppuccin Mocha"
 
 config.enable_tab_bar = false
